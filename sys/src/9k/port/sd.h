eb281667f4ff2a79b19a9127bceec2aaeaa87550
ericvh <ericvh@gmail.com>
2010-02-19 15:16:13 -0600

    hare: add AOE
    Add AOE support (to port instead of 386)
    
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- sys/src/9k/port/sd.h
+++ sys/src/9k/port/sd.h
@@ -135,3 +135,19 @@ extern int scsiverify(SDunit*);
 extern int scsionline(SDunit*);
 extern long scsibio(SDunit*, int, int, void*, long, vlong);
 extern SDev* scsiid(SDev*, SDifc*);
+
+/*
+ *  hardware info about a device
+ */
+typedef struct {
+	ulong	port;	
+	int	size;
+} Devport;
+
+struct DevConf
+{
+	ulong	intnum;			/* interrupt number */
+	char	*type;			/* card type, malloced */
+	int	nports;			/* Number of ports */
+	Devport	*ports;			/* The ports themselves */
+};
