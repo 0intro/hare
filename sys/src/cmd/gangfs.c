008c057a20e632b4699c537c79e4d1847a6c9e79
ericvh <ericvh@gmail.com>
2010-06-16 15:07:17 -0500

    gangfs: Initial file system skeleton
    This is the core piece of the new taskfs version.  It presents a 3 level
    hierarchy for gang operations.  Right now, this just implements the hierarchy,
    but will shortly be adding proper clone functionality and semantic routines
    to link it to both the multipipes and execfs.
    
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

---
+++ sys/src/cmd/gangfs.c
@@ -0,0 +1,402 @@
+/*
+	gangfs - aggregated execution management
+
+	Copyright (C) 2010, IBM Corporation, 
+ 		Eric Van Hensbergen (bergevan@us.ibm.com)
+
+	Description:
+		This provides a file system which interacts with execfs(4)
+		to provide a gang execution environment.  This initial version
+		will be single-node, but the intention is to support multinode
+		environments
+
+	Based in part on Pravin Shinde's devtask.c
+
+	TODO:
+		* lots
+*/
+
+#include <u.h>
+#include <libc.h>
+#include <fcall.h>
+#include <thread.h>
+#include <9p.h>
+#include <mp.h>
+#include <libsec.h>
+#include <stdio.h>
+
+char 	defaultpath[] =	"/proc";
+char *procpath;
+char *srvctl;
+Channel *iochan;
+int numsess = 1;
+int numsubsess = 5;
+
+char Enotdir[] = "Not a directory";
+char Enotfound[] = "File not found";
+char Eperm[] = "Permission denied";
+
+enum
+{
+	STACK = (8 * 1024),
+	NAMELEN = 255,
+};
+
+/* 
+  qid.path format:
+	[CONV(16)][TYPE(16)] where low 00ff bytes are type
+*/
+#define TYPE(x) 	 ((ulong) (x).path & 0xff)
+#define CONV(x) 	 ((((ulong) (x)->path >> 16)&0xffff) -1)
+#define CONVQIDP(c, y) (((c+1) << 16)|(y))
+
+typedef struct Dirtab Dirtab;
+struct Dirtab
+{
+	char name[NAMELEN];
+	Qid qid;
+	long perm;
+};
+
+/* TODO: review and see what we need and what we don't */
+enum
+{
+	Qroot = 0,
+	Qclone,
+	Qgstat,
+	Qrootend,
+	Qconv = 0,
+	Qctl,
+	Qenv,
+	Qns,
+	Qargs,
+	Qstatus,
+	Qwait,
+	Qstdin,
+	Qstdout,
+	Qstderr,
+	Qconvend,
+	Qsubses = Qconvend,
+};
+
+/* these dirtab entries form the base */
+static Dirtab rootdir[] = {
+	"clone", 	{Qclone},				0666,
+	"status",	{Qgstat},				0666,
+};
+
+static Dirtab convdir[] = {
+	"ctl",		{Qctl},					0666,
+	"env",		{Qenv},					0444,
+	"ns",		{Qns},					0444,
+	"args",		{Qargs},				0444,
+	"status",	{Qstatus},				0444,
+	"wait",		{Qwait},				0444,
+	"stdin",	{Qstdin},				0222,
+	"stdout",	{Qstdout},				0444,
+	"stderr",	{Qstderr},				0444,
+};
+
+static void
+usage(void)
+{
+	fprint(2, "gangfs [-D] [mtpt]\n");
+	exits("usage");
+}
+
+static void
+cleanup(Srv *)
+{
+	threadexitsall("done");
+}
+
+static void
+fsattach(Req *r)
+{
+	if(r->ifcall.aname && r->ifcall.aname[0]){
+		respond(r, "invalid attach specifier");
+		return;
+	}
+	r->fid->qid.path = Qroot;
+	r->fid->qid.type = QTDIR;
+	r->fid->qid.vers = 0;
+	r->ofcall.qid = r->fid->qid;
+	respond(r, nil);
+}
+
+static int
+dirgen(int n, Dir *d, void *aux)
+{
+	Qid *q = aux;
+	Dirtab *dt;
+	int ne = 0;
+	int s = 0;
+
+	USED(s);
+	USED(ne);
+
+	d->atime = time(nil);
+	d->mtime = d->atime;
+	d->uid = estrdup9p("gangfs");
+	d->gid = estrdup9p("gangfs");
+	d->muid = estrdup9p("gangfs");
+	d->length = 0;
+
+	if(n==-1) {
+		if(CONV(q) == -1) { /* root directory */
+			d->qid.path=CONVQIDP(-1, Qroot);
+			d->qid.type=QTDIR;
+			d->mode = 0555|DMDIR;
+			d->name = smprint("#gtaskfs");
+			return 0;			
+		} else {			/* conv directory */
+			d->qid.path=CONVQIDP(CONV(q), Qconv);
+			d->qid.type=QTDIR;
+			d->mode = 0555|DMDIR;
+			d->name = smprint("g%lud", CONV(q));
+			return 0;			
+		}
+	}
+
+	if(CONV(q) == -1) { /* root directory */
+		dt = rootdir;
+		ne = Qrootend-1;
+		s = n-ne;
+		if((s >= 0) && (s < numsess)) {
+			d->qid.path=CONVQIDP(s, Qconv);
+			d->qid.type=QTDIR;
+			d->mode = 0555|DMDIR;
+			d->name = smprint("g%d", n-ne);
+			return 0;
+		}
+	} else {				/* session directory */
+		dt = convdir;
+		ne = Qconvend-1;
+		s = n-ne;
+		if((s >= 0) && (s < numsubsess)) {
+			d->qid.path=CONVQIDP(CONV(q), Qsubses+s);
+			d->qid.vers=0;
+			d->qid.type=QTDIR;
+			d->mode = DMDIR;	/* mount points only! */
+			d->name = smprint("%d", s);
+			return 0;
+		}
+	}
+
+	if(n < ne) {	/* core synthetics */
+		d->qid.path = CONVQIDP(CONV(q), dt[n].qid.path);
+		d->mode = dt[n].perm;
+		d->name = estrdup9p(dt[n].name);
+		return 0;
+	}
+
+	return -1;
+}
+
+static int
+isanumber(char *thingy)
+{
+	if(thingy == nil)
+		return 0;
+	if((thingy[0] >=  '0')&&(thingy[0] <= '9'))
+		return 1;
+	return 0;
+}
+
+static void
+qidcopy(Qid *src, Qid *dst)
+{
+	dst->path = src->path;
+	dst->vers = src->vers;
+	dst->type = src->type;
+}
+
+static char*
+fswalk1(Fid *fid, char *name, Qid *qid)
+{
+	Dirtab *dt;
+	int j;
+
+	if(!(fid->qid.type&QTDIR))
+		return Enotdir;
+
+	if(strcmp(name, "..") == 0){
+		if((CONV(&fid->qid) == -1) || (TYPE(fid->qid) == Qconv)) {
+			qid->path = Qroot;
+			qid->type = QTDIR;
+			qidcopy(qid, &fid->qid);
+			return nil;
+		} else {
+			qid->path = CONVQIDP(CONV(&fid->qid), Qconv);
+			qid->type = QTDIR;
+			qidcopy(qid, &fid->qid);
+			return nil;
+		}	
+	}
+	/* depending on path do something different */
+	if(CONV(&fid->qid) == -1) {
+		dt = rootdir;
+		for(j = 0; j < Qrootend; j++)
+			if(strcmp(dt[j].name, name) == 0) {
+				qidcopy(&dt[j].qid, qid);
+				qidcopy(qid, &fid->qid);
+				return nil;
+			}
+		if( (name[0] == 'g') && (isanumber(name+1))) { /* session directory */
+			int s = atoi(name+1);
+			if(s < numsess) {
+				qid->path = CONVQIDP(s, Qconv);
+				qid->vers = 0;
+				qid->type = QTDIR;
+				qidcopy(qid, &fid->qid);
+				return nil;
+			}
+		}
+	} else {
+		dt = convdir;
+		for(j = Qconv; j < Qconvend; j++)
+			if(strcmp(dt[j].name, name) == 0) {
+				qidcopy(&dt[j].qid, qid);
+				qid->path = CONVQIDP(CONV(&fid->qid), qid->path);
+				qidcopy(qid, &fid->qid);
+				return nil;
+			}
+		if(isanumber(name)) {
+			int s = atoi(name);
+			if(s < numsubsess) {
+				qid->path = CONVQIDP(CONV(&fid->qid), s+Qsubses);
+				qid->vers = 0;
+				qid->type = QTDIR;
+				qidcopy(qid, &fid->qid);
+				return nil;
+			}		
+		}	
+	}
+		
+	return Enotfound;
+}
+
+static void
+fsopen(Req *r)
+{
+	Fid *fid = r->fid;
+	Qid *q = &fid->qid;
+
+	if(TYPE(fid->qid) >= Qsubses) {
+		respond(r, Eperm);
+		return;
+	}
+	if (q->type&QTDIR){
+		respond(r, nil);
+		return;
+	}
+
+	/* TODO: much, much more */
+	respond(r, "not yet");
+}
+
+static void
+fsread(Req *r)
+{
+	Fid *fid = r->fid;
+	Qid *q = &fid->qid;
+
+	if (q->type&QTDIR) {
+		dirread9p(r, dirgen, q);
+		respond(r, nil);
+		return;
+	}
+
+	respond(r, "not yet");
+}
+
+static void
+fsstat(Req* r)
+{
+	Fid *fid = r->fid;
+	Qid *q = &fid->qid;
+	int n = TYPE(fid->qid) - 1;
+
+	if(TYPE(fid->qid) >= Qsubses) {
+		respond(r, Eperm);
+		return;
+	}
+
+	if (dirgen(n, &r->d, q) < 0)
+		respond(r, Enotfound);
+	else
+		respond(r, nil);
+}
+
+/* handle certain ops in a separate thread in original namespace */
+static void
+iothread(void*)
+{
+	Req *r;
+
+	threadsetname("gangfs-iothread");
+	for(;;) {
+		r = recvp(iochan);
+		if(r == nil)
+			threadexits("interrupted");
+
+		switch(r->ifcall.type){
+		case Topen:
+			fsopen(r);	
+			break;
+		case Tread:
+			fsread(r);			
+			break;
+		default:
+			fprint(2, "unrecognized io op %d\n", r->ifcall.type);
+			break;
+		}
+	}
+}
+
+static void
+ioproxy(Req *r)
+{
+	if(sendp(iochan, r) != 1) {
+		fprint(2, "iochan hungup");
+		threadexits("iochan hungup");
+	}
+}
+
+Srv fs=
+{
+	.attach=	fsattach,
+	.walk1=	fswalk1,
+	.open=	ioproxy,
+	.write=	ioproxy,
+	.read=	ioproxy,
+	.stat	=	fsstat,
+	.end=	cleanup,
+};
+
+void
+threadmain(int argc, char **argv)
+{
+	ARGBEGIN{
+	case 'D':
+		chatty9p++;
+		break;
+	default:
+		usage();
+	}ARGEND
+
+	if(argc > 1)
+		usage();
+
+	if(argc)
+		procpath = argv[0];
+	else
+		procpath = defaultpath;
+
+	/* spawn off a io thread */
+	iochan = chancreate(sizeof(void *), 0);
+	proccreate(iothread, nil, STACK);
+
+	threadpostmountsrv(&fs, nil, procpath, MAFTER);
+	threadexits(0);
+}
