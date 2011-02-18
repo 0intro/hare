c27f535f15f1c293f77e2566fb74cf795b3ba671
rminnich <rminnich@gmail.com>
2011-02-18 17:16:58 +0000

    a few more test programs.

--- sys/src/cmd/rompi/hello.c
+++ sys/src/cmd/rompi/hello.c
@@ -6,9 +6,24 @@ extern int torusdebug, rompidebug;
 u64int	tbget(void);
 int node, nproc;
 
+unsigned int getr2 (void)
+{
+  unsigned int __sp__;
+  __asm__ __volatile__ ("mr %0, 2" : "=r" (__sp__));
+  return __sp__;
+}
+
 int
 main(int argc, char **argv)
 {
-	printf("Hello, from CNK\n");
+	unsigned int *p;
+	printf("Hello, from CNK r2 is 0x%x\n", getr2());
+	printf("pid %d\n", getpid());
+	p = malloc(2048);
+	printf("p %p\n", p);
+	free(p);
+	p = malloc(412);
+	printf("p %p\n", p);
+	
 	return 0;
 }
