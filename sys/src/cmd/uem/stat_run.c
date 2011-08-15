a427145c329b94475b881f39651e03a63691a954
John (EBo) David <ebo@users.sourceforge.net>
2011-08-15 09:18:08 -0500

    added optional tmp file and num_tasks /2 loop (for multiple testing).
    lots of other minor updates for timing testing

--- sys/src/cmd/uem/stat_run.c
+++ sys/src/cmd/uem/stat_run.c
@@ -55,7 +55,8 @@ main(int argc, char **argv)
 {
 	char *command, *procpath="/n/io/proc";
 	//char *tmpfile="/tmp/tmp1";
-	char *tmpfile="tests/.test/tmp1";
+	//char *tmpfile="tests/.test/tmp1";
+	char *tmpfile=nil;
 	char *srvnm="io";
 	char *log_file=nil;
 	int i, num_tasks=1;
@@ -82,7 +83,7 @@ main(int argc, char **argv)
 		loop = 1;
 		break;
 	case 't':
-		tmpfile = ARGF();
+		tmpfile = estrdup9p(ARGF());
 		break;
 	case 'N':
 		num_tasks = atoi(ARGF());
@@ -102,79 +103,93 @@ main(int argc, char **argv)
 	}
 
 	DPRINT(DARG, "Main: command=(%s)\n", command);
-	DPRINT(DARG, "\ttmpfile=(%s)\n", tmpfile);
+	DPRINT(DARG, "\ttmpfile=(%s)\n", (tmpfile!=nil)?tmpfile:"<nil>");
 	DPRINT(DARG, "\tprocpath=(%s)\n", procpath);
 	DPRINT(DARG, "\tnum_tasks=(%d)\n", num_tasks);
 
 	mountgang(srvnm, "/n/io", "");
 
+/*
+uvlong ftime, stime;
+uvlong ttime;
+slice(ftime);
+sleep(10000);
+slice(stime);
+ttime = (stime-ftime)/10;
+fprint(2,"f/stime = %ulld\n", ttime);
+*/
 
 	timeit_init();
+	timeit_settype(TIMIT_DEFAULT);
+		
 	if(log_file)
 		timeit_setout(log_file);
 
-for(loop=(loop==1)?num_tasks:0; loop>0; loop=(loop==1)?0:loop/2){
-
-	int gfdi, gfdo=-1;
-	snprint(line, 1024, "%s/gclone", procpath);
-	if((gfdi=open(line,ORDWR)) < 0){
-		DPRINT(DERR, "*ERROR*: could not open gclone file (%s)\n",  line);
-		exits("could not open gclone");
-
-	}
-
 	if(loop)
-		num_tasks = loop;
-
-	fprint(2, "num_tests=%d\n", num_tasks);
-	stamp("BEGIN");
-
-	fprint(gfdi, "res %d", num_tasks);
-
-	stamp("RES");
-
-	int gnum;
-	int n = read(gfdi, line, 256);
-	if(n<0){
-		DPRINT(DERR, "*ERROR*: read failed\n");
-		exits("could not read clone file");
-	}
-	gnum = atoi(line);
-	DPRINT(DFID, "Main: gclone number = g%d\n", gnum);
-
-	// FIXME: find a better way to find the gang ID.  R/W gfdi returns 0
-	int gpid;
-	for(gpid=0; gpid<100; gpid++){
-		snprint(gstdout, 1024, "%s/g%d/stdout", procpath, gpid);
-		gfdo = open(gstdout,OREAD);
-		if(gfdo > 0)
-			break;
-	}
-	if(gfdo < 0){
-		DPRINT(DERR, "*ERROR*: could not open stdout file (%s)\n", gstdout);
-		exits("could not open stdout");
+		loop = num_tasks;
+	if(num_tasks<=0){
+		DPRINT(DERR, "*ERROR*: num_tasks must be greater than 0\n");
+		exits("invalid num_tasks");
 	}
 
-	stamp("finish opening stdout");
+	do{
+		int gfdi, gfdo;
+		int gnum, n;
+		int tmpfd=1;
 
-
-	fprint(gfdi, "exec %s", command);
-	
-	stamp("EXEC");
-
-	sprint(line, "%s", tmpfile);
-	print("opening tmpfile %s\n", line);
-	int tmpfd;
-	if((tmpfd=open(line,OWRITE)) < 0){
-		if((tmpfd=create(line,OWRITE,0666)) < 0){
-			DPRINT(DERR, "*ERROR*: could not open tmpfile (%s)\n", line);
-			exits("could not open tmpfile");
+		snprint(line, 1024, "%s/gclone", procpath);
+		if((gfdi=open(line,ORDWR)) < 0){
+			DPRINT(DERR, "*ERROR*: could not open gclone file (%s)\n",  line);
+			exits("could not open gclone");
 		}
+		
+		if(loop)
+			num_tasks = loop;
+		
+		print("tasks = %d\n", num_tasks);
 
-	}
-
+		n = read(gfdi, line, 256);
+		if(n<0){
+			DPRINT(DERR, "*ERROR*: read failed\n");
+			exits("could not read clone file");
+		}
+		line[n] = 0;
+		gnum = atoi(line);
+		DPRINT(DFID, "Main: gclone number = g%d\n", gnum);
+		DPRINT(DFID, "\tnum_tests=%d\n", num_tasks);
+		stamp("BEGIN");
+		
+		fprint(gfdi, "res %d", num_tasks);
+		
+		stamp("RES");
+		
+		snprint(gstdout, 1024, "%s/g%d/stdout", procpath, gnum);
+		gfdo = open(gstdout,OREAD);
+		if(gfdo < 0){
+			DPRINT(DERR, "*ERROR*: could not open stdout file (%s)\n", gstdout);
+			exits("could not open stdout");
+		}
+		
+		stamp("finish opening stdout");
+		
+		
+		fprint(gfdi, "exec %s", command);
+		
+		stamp("EXEC");
+		
+		if(tmpfile!=nil){
+			sprint(line, "%s", tmpfile);
+			print("opening tmpfile %s\n", line);
+			if((tmpfd=open(line,OWRITE|OTRUNC)) < 0){
+				if((tmpfd=create(line,OWRITE,0666)) < 0){
+					DPRINT(DERR, "*ERROR*: could not open tmpfile (%s)\n", line);
+					exits("could not open tmpfile");
+				}
+			}
+		}
+		
 /*
-  // FIXME: bind sais this is inconsistent
+	// FIXME: bind sais this is inconsistent
 	if(tmpfile!=nil){
 		int ret;
 		//close(gfdo);
@@ -183,50 +198,56 @@ for(loop=(loop==1)?num_tasks:0; loop>0; loop=(loop==1)?0:loop/2){
 			DPRINT(DERR, "bind failed: %r\n");
 			exits("bind failed");
 		}
-	}
+       	}
 	dup(gfdo, 1);
 */
 
-	dup(tmpfd, 1);
+//		dup(tmpfd, 1);
+
 //	int p[2];
 //	pipe(p);
 //	dup(p[1], 1);
 //	close(p[0]);
 //	close(p[1]);
+		
+		// FIXME: there is a better way to do this than here...
+		DPRINT(DFID, "num_tasks=(%d) for output\n", num_tasks);
+		for(i=0; i<num_tasks; i++){
+			n = read(gfdo, line, 1024);
+			if(n<=0) break;
+			
+			n = write(tmpfd, line, n);
+			if(n<=0) break;
+		};
+		
+		stamp("OUT");
+		
+		stamp("TERM");
+		
+		timeit_setfd(2);
+		timeit_end();
 
-/**/
-	// FIXME: there is a better way to do this than here...
-	for(i=0; i<num_tasks; i++){
-		n = read(gfdo, line, 1024);
-		if(n<=0) break;
+		timeit_settype(TIMIT_FLOAT);
 
-		n = write(1, line, n);
-		if(n<=0) break;
-	};
-/**/
+		timeit_tag_dt("BEGIN", "RES", "Time for stat_run RES");
+		timeit_tag_dt("finish opening stdout", "EXEC", "Time for EXEC");
+		timeit_tag_dt("EXEC", "OUT", "Time for OUT");
+		timeit_tag_dt("OUT", "TERM", "Time for TERM");
+		
+		timeit_tag_dt("BEGIN", "OUT", "Total time");
+		
+		if(tmpfd>1)
+			close(tmpfd);
+		close(gfdi);
+		close(gfdo);
+		
+		timeit_dump();
+		fprint(2, "\n");
 
-	stamp("OUT");
+		loop /= 2;
+		num_tasks = loop;
+		
+	} while (num_tasks>0);
 
-	stamp("TERM");
-
-	timeit_setfd(2);
-	timeit_end();
-
-	timeit_settype(TIMIT_FLOAT);
-
-	timeit_tag_dt("BEGIN", "RES", "Time for RES");
-	timeit_tag_dt("finish opening stdout", "EXEC", "Time for EXEC");
-	timeit_tag_dt("EXEC", "OUT", "Time for OUT");
-	timeit_tag_dt("OUT", "TERM", "Time for TERM");
-
-	timeit_tag_dt("BEGIN", "OUT", "Total time");
-
-	close(tmpfd);
-
-	timeit_dump();
-	fprint(2, "\n");
-
-} // end for loop
-
+	exits(0);
 }
-
